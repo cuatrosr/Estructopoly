ceba71e1477c8d89a2bc42d9af12027301e844c3
Samuel Hernandez <doicirohangnan@gmail.com>
2021-11-13 12:57:49 -0500

    Finished board fxml outline and token selection fxml

--- src/ui/FXMainController.java
+++ src/ui/FXMainController.java
@@ -1,29 +1,60 @@
 package ui;
 
 import com.jfoenix.controls.JFXToggleButton;
+import com.jfoenix.controls.JFXToggleNode;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.effect.Glow;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class FXMainController {
+public class FXMainController implements Initializable {
 
     @FXML
-    private BorderPane mainPane;
+    private BorderPane mainPane = new BorderPane();
 
     @FXML
-    private JFXToggleButton fullscreenTGB;
+    private JFXToggleButton fullscreenTGB = new JFXToggleButton();
+
+    boolean glowApplied;
+
+    FXTokens fxTokens;
+
+    public FXMainController() {
+        fxTokens = new FXTokens(this);
+        glowApplied = false;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        if (glowApplied) {
+            fullscreenTGB.setSelected(true);
+            auxToggleGlow(true);
+        }
+    }
 
     @FXML
     void toggleFullscreen(ActionEvent event) {
-        boolean fs = fullscreenTGB.isSelected();
-        ((Stage) fullscreenTGB.getScene().getWindow()).setFullScreen(fs);
+        boolean fullscreen = fullscreenTGB.isSelected();
+        glowApplied = fullscreen;
+        Stage curr = ((Stage) fullscreenTGB.getScene().getWindow());
+        curr.setFullScreen(fullscreen);
+        auxToggleGlow(fullscreen);
+    }
+
+    void auxToggleGlow(boolean fullscreen) {
         for (Node node : fullscreenTGB.getChildrenUnmodifiable()) {
             if (!(node instanceof Text)) {
-                double glowVal = (fs) ? 1 : 0;
+                double glowVal = (fullscreen) ? 1 : 0;
                 node.setEffect(new Glow(glowVal));
             }
         }
@@ -31,7 +62,14 @@ public class FXMainController {
 
     @FXML
     void play(ActionEvent event) {
-
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/select-players.fxml"));
+            fxmlLoader.setController(fxTokens);
+            Parent root = fxmlLoader.load();
+            mainPane.setCenter(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
@@ -41,6 +79,10 @@ public class FXMainController {
 
     @FXML
     void exit(ActionEvent event) {
+        Platform.exit();
+    }
 
+    public BorderPane getMainPane() {
+        return mainPane;
     }
 }
---
+++ src/ui/FXTokens.java
@@ -0,0 +1,74 @@
+package ui;
+
+import com.jfoenix.controls.JFXToggleNode;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+
+public class FXTokens {
+
+    @FXML
+    private BorderPane tokensPane = new BorderPane();
+
+    @FXML
+    private JFXToggleNode token1 = new JFXToggleNode();
+
+    @FXML
+    private JFXToggleNode token2 = new JFXToggleNode();
+
+    @FXML
+    private JFXToggleNode token3 = new JFXToggleNode();
+
+    @FXML
+    private JFXToggleNode token4 = new JFXToggleNode();
+
+    @FXML
+    private JFXToggleNode token5 = new JFXToggleNode();
+
+    @FXML
+    private JFXToggleNode token6 = new JFXToggleNode();
+
+    @FXML
+    private JFXToggleNode token7 = new JFXToggleNode();
+
+    @FXML
+    private JFXToggleNode token8 = new JFXToggleNode();
+
+    private FXMainController mainController;
+
+    private FXBoard fxBoard;
+
+    public FXTokens(FXMainController mainController) {
+        this.mainController = mainController;
+        fxBoard = new FXBoard(mainController);
+    }
+
+    @FXML
+    void back(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/menu.fxml"));
+            fxmlLoader.setController(mainController);
+            Parent root = fxmlLoader.load();
+            mainController.getMainPane().setCenter(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void loadBoard(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/board.fxml"));
+            fxmlLoader.setController(fxBoard);
+            Parent root = fxmlLoader.load();
+            mainController.getMainPane().setCenter(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
